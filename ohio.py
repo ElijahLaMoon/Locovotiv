1ea7c9d14c0825463589bc0885fe2f2d7194be2b
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2020-06-18 17:20:23 -0400

    created prototype for entire ohio bot

--- ohio.py
+++ ohio.py
@@ -6,9 +6,11 @@ import sys
 
 start = time.time()
 DATE = dt.now()
-# use name and office level
+
+# use name, office level, and office
 NAME = sys.argv[1].strip().title()
 OFFICE_LEVEL = sys.argv[2].strip().title()
+OFFICE = sys.argv[3].strip().title()
 
 # create a crawler
 crawler = CampaignCrawler()
@@ -21,3 +23,11 @@ running_years = crawler.get_running_years(candidate)
 data_paths = [crawler.download_campaign_data(year, OFFICE_LEVEL) for year in running_years]
 
 # analyze each data path with a campaign analyzer
+analysts = [CampaignAnalyzer(path) for path in data_paths]
+
+# get office frames for all of the years --> use OG NAME
+office_frames = [analyst.get_office_data(OFFICE_LEVEL) for analyst in analysts]
+
+# export the office frames one by one
+for frame in office_frames:
+    frame.export_candidate_data(NAME)
